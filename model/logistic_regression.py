51580938de2024efb6fd0c713fa5600057697eae
polusy <marchisella.p@gmail.com>
2026-02-06 20:14:18 +0100

    adding the binary class. function

--- model/logistic_regression.py
+++ model/logistic_regression.py
@@ -4,8 +4,8 @@ import numpy as np
 class linear_regression_model :
 
     def __init__(self, parameters, bias):
-        _parameters = parameters
-        _bias = bias
+        self._parameters = parameters
+        self._bias = bias
 
     def predict(self, features_instances):
 
@@ -15,8 +15,8 @@ class linear_regression_model :
 
         #computing the product between each feature instance
         #and each corrispondent parameter, store it in prediction
-        while(i < features_instances.size()):
-            prediction += features_instances.get(i)*self._parameters.get(i)
+        for i in range(len(features_instances)):
+            prediction += features_instances[i]*self._parameters[i]
 
         return prediction
 
@@ -33,3 +33,20 @@ class logistic_regression_model :
     #logistic transformation of a regression model prediction
     def logistic_trans(regression_pred):
         return 1/(1 + np.exp(-regression_pred))
+    
+    #using the previous logistic transformation
+    #to return the prediction
+    def predict(self, regression_pred):
+        return self.logistic_trans(regression_pred)
+    
+
+    #discretizing the continuous prediction
+    def binary_classify(self, regression_pred):
+        continuous_pred = self.predict(regression_pred)
+        
+        #creating the threshold
+        if(continuous_pred < 0.5):
+            return 0
+        else:
+            return 1
+        
