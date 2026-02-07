25b759b90cb5a3482c89a3ed448896a2267dfd3a
polusy <marchisella.p@gmail.com>
2026-02-07 14:25:05 +0100

    changed data_reader in data_manipulator

---
+++ utils/data_manipulator.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+class DataNormalizer:
+    #uso reader di pandas, quindi prendo dataframe e faccio
+    #normalizzazione standard di ogni feature numerica,
+    #il normalizzatore deve prendere 
+    #prima media di ogni colonna e deviazione standard di ogni colonna
+    #del set di training, e normalizzare set di training e set di test
+    #con questi valori.
+    pass
+
+
+class DataSplitter:
+    #implementare uno splitter di dati, in dati di test e in 
+    #dati di training, restituendo file in formato CSV, nel
+    #path /parkinson-analysis/data
+    pass
+
+
