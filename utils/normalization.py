fe65968a4d65f8e736ffc4b906c0adedd4ea12a8
polusy <marchisella.p@gmail.com>
2026-02-08 14:11:44 +0100

    added the normalization file script

---
+++ utils/normalization.py
@@ -0,0 +1,6 @@
+from preprocessing import DataNormalizer
+
+
+#script in cui si chiama il metodo di DataNormalizer
+# e si producono effettivamente i due csv normalizzati e si 
+#inseriscono nel folder data
