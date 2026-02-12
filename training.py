c613831caa9493618b96d786d5f4501715c3525e
polusy <marchisella.p@gmail.com>
2026-02-12 20:22:27 +0100

    started first building blocks for training weak learners

---
+++ training.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+
+
+class LogisticRegressorTraining:
+    def fit(csv_training):
+        pass
+    pass
+
+
+
+class LinearRegressorTraining:
+
+    def fit(csv_training):
+        pass
+    pass
+
+
