3fcd2646a0a9928179b7cd52ce2703623d64b30a
polusy <marchisella.p@gmail.com>
2026-02-14 15:13:08 +0100

    added weak_learners_validation file

---
+++ validation/weak_learners_validation.py
@@ -0,0 +1,20 @@
+from sklearn.model_selection import GroupKFold
+
+
+class RegularizationHyperParameterCrossValidation:
+
+    """this class has the purpose of finding the regularization hyperparameter of
+    the first weak learner (logistic regressor) and the regularization hyperparameter 
+    of the other weak learners (linear regressors)"""
+
+    def log_regressor_reg_hyperparam_cross_validate(logistic_regressor, reg_hyperparam_list, training_dataframe):
+        
+        
+
+
+
+
+
+    def lin_regressior_reg_hyperparam_cross_validate(logistic_regressor, linear_regressor, reg_hyperparam_list, training_dataframe):
+        pass
+
