f05fbdae8fb383bbc89036b4537edc5305ed9155
polusy <marchisella.p@gmail.com>
2026-02-14 16:09:04 +0100

    created the linear regression test function

--- test.py
+++ test.py
@@ -39,5 +39,31 @@ class LogisticRegressorTest:
 
 
 class LinearRegressorTest:
+
     def test(linear_regressor, test_dataframe):
-        pass
+
+
+        #creating the input features dataframe and the target feature dataframe in order to test the 
+        #test set of examples
+        input_features_dataframe, target_feature_dataframe = DataSplitter.split_targets_from_input(test_dataframe)
+
+
+        prediction_cumulative_squared_error = 0
+
+        #iterating over the examples in the test dataframe
+        for (example_index,example_features), (target_index,example_target) in zip(input_features_dataframe.iterrows(), target_feature_dataframe.items()):
+
+            vectorized_example_features = example_features.to_numpy()
+
+            #computing the logistic regressor prediction and the Squared Error between
+            #the target prediction and the target real value, cumulating it in a variable
+            target_prediction = linear_regressor.predict(vectorized_example_features)
+            prediction_cumulative_squared_error += (target_prediction - example_target)**2
+
+
+        #to compute the MSE, take the cumulative Squared Error and divide it 
+        #by the number of rows in the test_dataframe
+        mean_squared_error = prediction_cumulative_squared_error/len(test_dataframe)
+
+
+        return mean_squared_error
