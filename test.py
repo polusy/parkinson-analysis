342beae362e8c23c8b97ea66f29b755d50f23e57
polusy <marchisella.p@gmail.com>
2026-02-14 17:51:11 +0100

    builded a prediction residuals dataframe creation function

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from utils.preprocessing import DataSplitter
 import numpy as np
+import pandas as pd
 
 
 
@@ -31,6 +32,28 @@ class LogisticRegressorTest:
 
 
         return mean_log_loss
+    
+    
+    
+
+    def create_prediction_residuals_dataframe(logistic_regressor, test_dataframe):
+        
+        #creating the input features dataframe and the target feature dataframe in order to test the 
+        #test set of examples
+        input_features_dataframe, target_feature_dataframe = DataSplitter.split_targets_from_input(test_dataframe)
+
+        #iterating over the examples in the test dataframe
+        for (example_index,example_features), (target_index,example_target) in zip(input_features_dataframe.iterrows(), target_feature_dataframe.items()):
+
+            vectorized_example_features = example_features.to_numpy()
+
+            #computing dhe difference between log regressor prediction and example target
+            target_prediction = logistic_regressor.predict(vectorized_example_features)
+            example_target = example_target - target_prediction
+
+
+        #return the complete dataframe, with target value as residuals
+        return pd.concat([input_features_dataframe, target_feature_dataframe], axis = 1)
 
 
 
