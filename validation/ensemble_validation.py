ef97e2553548778acca2b9784bf6566b8ef7f0d2
polusy <marchisella.p@gmail.com>
2026-02-15 19:45:48 +0100

    created ensemble model validation class

---
+++ validation/ensemble_validation.py
@@ -0,0 +1,8 @@
+from model import ensemble
+
+
+class BoostingRoundsCrossValidation:
+
+    def ensemble_model_boosting_rounds_cross_validate(ensemble_model, training_dataframe):
+        pass
+    
