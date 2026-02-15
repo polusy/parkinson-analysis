b401ceb8c0d3aeeb6022d88a0da1ce0c12e12a93
polusy <marchisella.p@gmail.com>
2026-02-15 20:06:48 +0100

    added useful constants to constants file

--- utils/constants.py
+++ utils/constants.py
@@ -1,2 +1,14 @@
 
+#number of instance features for the current classification problem
 NUM_INSTANCE_FEATURES = 22
+
+
+#regularization hyperparameters, found after cross-validation
+LOGISTIC_REG_HYPERPARAMETER = 0
+LINEAR_REG_HYPERPARAMETER = 0
+
+#learning rate hyperparameter (fixed, not found with cross-validation)
+LEARNING_RATE = 0.01
+
+#nubers of batches for model training
+BATCHES_NUM = 5
