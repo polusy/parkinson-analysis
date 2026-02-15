b75468208bcb61a709cfd245ab98b63eed257e1b
polusy <marchisella.p@gmail.com>
2026-02-15 18:31:18 +0100

    added the fit methods to regression model classes

--- model/logistic_regression.py
+++ model/logistic_regression.py
@@ -1,4 +1,6 @@
+from numpy import linalg,subtract
 import numpy as np
+import pandas as pd
 
 
 class LinearRegressionModel :
@@ -29,6 +31,115 @@ class LinearRegressionModel :
 
         return prediction
     
+
+    def fit(self, reg_hyperparameter, batches_num, learning_rate, training_dataframe):
+
+            """fitting the linear regressor to a specific training dataset
+            given a regularization hyperparameter, the number of batches to be trained on
+            and the learning rate of the gradient descent"""
+
+            #creating batches of samples (mini-dataframes), given a requested number of batches
+            #dont care about same patient samples distributed over different batches
+            batches = np.array_split(training_dataframe, batches_num)
+
+
+            #splitting the batches in input features batches and target feature batches
+            input_features_batches = [batches[i].drop(['name', 'status']) for i in range(len(batches))]
+            target_feature_batches = [batches[i]['status'] for i in range(len(batches))]
+
+            #initializing the loss gradient and the gradient tollerance value
+            #the dimension of the gradient is taken from the number
+            #of columns of a random batch in the input_features_batches
+            loss_gradient = [100 for i in range(len(input_features_batches[0].columns) + 1)]
+            epoch_gradient = [0 for i in range(len(input_features_batches[0].columns) + 1)]
+            
+
+            #initialiazing the tollerance values
+            parameters_subtraction_norm_tol = 10**-3
+            gradient_norm_tol = 10**-3
+
+            #initializing the first with real parameters values and the second with high values
+            #such that at first, verifying the condition in the loop, their subtraction is high
+            parameters_before_epoch = self._parameters
+            parameters_after_epoch = [100 for i in range(len(parameters_before_epoch))]
+
+            #iterating in epochs if 
+            # - the gradient norm is still too high
+            # - the norm of the subtraction between parameters after and befor epoch is too high
+            while (linalg.norm(np.array(epoch_gradient)) > gradient_norm_tol) and linalg.norm(subtract(parameters_after_epoch, parameters_before_epoch)) > parameters_subtraction_norm_tol :
+
+                #resetting epoch_gradient at the start
+                epoch_gradient = [0 for i in range(len(input_features_batches[0].columns) + 1)]
+                
+                #saving the model parameters before starting the training epoch
+                parameters_before_epoch = self._parameters
+
+                #iterating over input features and target feature batches
+                for input_feature_batch, target_feature_batch in zip(input_features_batches, target_feature_batches):
+
+                    current_batch_examples_num = len(input_feature_batch)
+
+                    #re-setting the 0 value for loss gradient, adding 1 to range to include
+                    #the partial derivative of loss to respect with bias
+                    loss_gradient = [0 for i in range(len(input_features_batches[0].columns) + 1)]
+
+                    #iterating over the example in the batch
+                    for (example_index,example_features), (target_index,example_target) in zip(input_feature_batch.iterrows(), target_feature_batch.items()):
+
+                        vectorized_example_features = example_features.to_numpy()
+
+                        #computing the linear regressor prediction and the loss between the target value
+                        #and the target prediction
+                        target_prediction = self.predict(vectorized_example_features)
+                        example_prediction_loss = target_prediction - example_target 
+
+                        #iterating over the example features to calculate the partial derivative
+                        #of loss with respect to each parameter in the regression model
+                        for i in range(len(vectorized_example_features) + 1):
+
+                            #distinguish the partial derivative of mean squared loss with respect to bias (last parameter)
+                            #from the partial derivatives of loss with respect to other parameters
+                            if i == len(vectorized_example_features):
+                                loss_gradient[i] += 2*example_prediction_loss #derivative of MSE with respect to bias
+                            else:
+                                loss_gradient[i] += 2*example_prediction_loss*vectorized_example_features[i] #derivative of MSE with respect to other parameters
+
+                    #taking the mean partial derivative of loss with respect to each parameter
+                    #to compute partial derivates of mean log loss
+                    for i in range(len(vectorized_example_features) + 1):
+                        loss_gradient[i] /= current_batch_examples_num
+                        epoch_gradient[i] += loss_gradient[i]
+
+                    
+                    #updating linear regression model parameters
+                    for i in range(len(parameters_before_epoch)):
+
+                        #taking the previous value of the parameter
+                        parameter_i = self._parameters[i]
+
+                        #updating the parameter subtracting from it the loss gradient 
+                        self._parameters[i] = parameter_i - learning_rate*loss_gradient[i]
+
+                        #the bias term is not updated with regularization
+                        if i != len(parameters_after_epoch) - 1:
+
+                            #computing the L2 regularization term
+                            L2_reg_term = learning_rate* ((reg_hyperparameter)/current_batch_examples_num)*parameter_i
+
+                            #new update for L2 regularization, subtracting from the previously updated parameter the L2 reg. term
+                            updated_parameter_i = self._parameters[i]
+                            self._parameters[i] = updated_parameter_i - L2_reg_term
+
+
+                #updating the epoch gradient with the mean of gradients over all the batches 
+                epoch_gradient = [ddw_sum/batches_num for ddw_sum in epoch_gradient]
+
+                #rescuing the parameters after an epoch
+                parameters_after_epoch = self._parameters
+
+
+                
+    
     #defining getters and setters
     def get_bias(self):
         return self._bias
@@ -36,7 +147,7 @@ class LinearRegressionModel :
     #returning value of a specific parameter
     #of given index
     def get_parameter(self, param_index):
-        return self._parameter[param_index]
+        return self._parameters[param_index]
     
     def get_parameters(self):
         return self._parameters
@@ -66,6 +177,114 @@ class LogisticRegressionModel :
     def sigmoid(regression_pred):
         return 1/(1 + np.exp(-regression_pred))
     
+    
+    def fit(self, reg_hyperparameter, batches_num, learning_rate, training_dataframe):
+
+        """fitting the logistic regressor to a specific training dataset
+        given a regularization hyperparameter, the number of batches to be trained on
+        and the learning rate of the gradient descent"""
+
+        #creating batches of samples (mini dataframe), given a requested number of batches
+        #dont care about same patient samples distributed over different batches
+        batches = np.array_split(training_dataframe, batches_num)
+
+
+        #splitting the batches in input features batches and target feature batches
+        input_features_batches = [batches[i].drop(['name', 'status']) for i in range(len(batches))]
+        target_feature_batches = [batches[i]['status'] for i in range(len(batches))]
+
+        #initializing the loss gradient and the gradient tollerance value
+        #the dimension of the gradient is taken from the number
+        #of columns of a random batch in the input_features_batches
+        loss_gradient = [100 for i in range(len(input_features_batches[0].columns) + 1)]
+        epoch_gradient = [0 for i in range(len(input_features_batches[0].columns) + 1)]
+        
+
+        #initialiazing the tollerance values
+        parameters_subtraction_norm_tol = 10**-3
+        gradient_norm_tol = 10**-3
+
+        #initializing the first with real parameters value and the second with high values
+        #such that at the first loop condition check, their subtraction is high
+        parameters_before_epoch = self._regression_model.get_parameters()
+        parameters_after_epoch = [100 for i in range(len(parameters_before_epoch))]
+
+        #iterating in epochs if 
+        # - the gradient norm is still too high
+        # - the norm of the subtraction between parameters after and befor epoch is too high
+        while (linalg.norm(np.array(epoch_gradient)) > gradient_norm_tol) and linalg.norm(subtract(parameters_after_epoch, parameters_before_epoch)) > parameters_subtraction_norm_tol :
+
+            #resetting epoch_gradient at the start
+            epoch_gradient = [0 for i in range(len(input_features_batches[0].columns) + 1)]
+            
+            #saving the model parameters before starting the training epoch
+            parameters_before_epoch = self._regression_model.get_parameters()
+
+            #iterating over input features and target feature batches
+            for input_feature_batch, target_feature_batch in zip(input_features_batches, target_feature_batches):
+
+                current_batch_examples_num = len(input_feature_batch)
+
+                #re-setting the 0 value for loss gradient, adding 1 to range to include
+                #the partial derivative of loss to respect with bias
+                loss_gradient = [0 for i in range(len(input_features_batches[0].columns) + 1)]
+
+                #iterating over the example in the batch
+                for (example_index,example_features), (target_index,example_target) in zip(input_feature_batch.iterrows(), target_feature_batch.items()):
+
+                    vectorized_example_features = example_features.to_numpy()
+
+                    #computing the logistic regressor prediction and the loss between the target value
+                    #and the target prediction
+                    target_prediction = self.predict(vectorized_example_features)
+                    example_prediction_loss = target_prediction - example_target 
+
+                    #iterating over the example features to calculate the partial derivative
+                    #of loss with respect to each parameter in the regression model
+                    for i in range(len(vectorized_example_features) + 1):
+
+                        #distinguish the partial derivative of loss with respect to bias (first parameter)
+                        #from the partial derivatives of loss with respect to other parameters
+                        if i == len(vectorized_example_features):
+                            loss_gradient[i] += example_prediction_loss #derivative with respect to bias
+                        else:
+                            loss_gradient[i] += example_prediction_loss*vectorized_example_features[i]
+
+                #taking the mean partial derivative of loss with respect to each parameter
+                #to compute partial derivates of mean log loss
+                for i in range(len(vectorized_example_features) + 1):
+                    loss_gradient[i] /= current_batch_examples_num
+                    epoch_gradient[i] += loss_gradient[i]
+
+                
+                #updating linear regression model parameters
+                for i in range(len(parameters_before_epoch)):
+
+                    #taking the previous value of the parameter
+                    parameter_i = self._regression_model.get_parameter(i)
+
+                    #updating the parameter subtracting from it the loss gradient 
+                    self.set_parameter(i, parameter_i - learning_rate*loss_gradient[i])
+
+                    #do not udate the bias term with regularization
+                    if i != len(parameters_after_epoch) - 1:
+                        
+                        #computing the L2 regularization term
+                        L2_reg_term = learning_rate* ((reg_hyperparameter)/current_batch_examples_num)*parameter_i
+
+                        #new update for L2 regularization, subtracting from the previously updated parameter the L2 reg. term
+                        updated_parameter_i = self._regression_model.get_parameter(i)
+                        self.set_parameter(i, updated_parameter_i - L2_reg_term)
+
+
+            #updating the epoch gradient with the mean of gradient (partial derivatives' vector) over all the batches 
+            epoch_gradient = [ddw_sum/batches_num for ddw_sum in epoch_gradient]
+
+            #rescuing the parameters after an epoch
+            parameters_after_epoch = self._regression_model().get_parameters()
+    
+
+
 
     #using the previous logistic transformation
     #and the linear model stored 
