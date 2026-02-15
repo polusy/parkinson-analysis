df7dd959cfe724cbf8127316212ddecd2798606d
polusy <marchisella.p@gmail.com>
2026-02-15 16:12:26 +0100

    changed boosting_model name to ensemble

---
+++ model/ensemble.py
@@ -0,0 +1,16 @@
+
+from logistic_regression import LogisticRegressionModel,LinearRegressionModel
+
+class GradientBoostingModel:
+
+    #istanziato come lista di regressori lineari + primo regressore logistico
+
+    #predict : passo dal primo regressore logistico
+    #uso valore predetto, e ci aggiungo tutte le predizioni
+    #in sequenza degli altri regressori lineari
+
+
+    #training : 
+
+    pass
+
