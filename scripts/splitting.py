7115874050143588279dea618f78b3030ac8dc02
polusy <marchisella.p@gmail.com>
2026-02-16 22:36:56 +0100

    added init to each folder

--- scripts/splitting.py
+++ scripts/splitting.py
@@ -1,5 +1,8 @@
-from preprocessing import DataSplitter
+from utils.preprocessing import DataSplitter
 
 #script in cui si chiama il metodo di DataSplitter
 # e si producono effettivamente i due csv splitted in training_raw
 # e test_raw
+
+
+DataSplitter.split("data/parkinsons_raw.data", 0.2)
