056d11f260c7bd2184cfde612e45b32c8851750f
polusy <marchisella.p@gmail.com>
2026-02-17 18:12:11 +0100

    added boosting rounds cross validation script

--- scripts/cross_validation.py
+++ scripts/cross_validation.py
@@ -8,21 +8,21 @@ from utils.constants import LOGISTIC_REG_HYPERPARAMETER
 #using the same regularization hyperparameter list, because we use L2 reg. in both 
 #the log. regression model and the linear regression model
 reg_hyperparameter_list = [0.001, 0.1, 1.0, 10.0]
-boosting_rounds_num_list = [3,5,10,20]
+boosting_rounds_num_list = [2,3,5]
 
-"""#finding the regularization hyperparameter that reduces mean and std. dev log loss. using 
+#finding the regularization hyperparameter that reduces mean and std. dev log loss. using 
 #k-folds cross validation approach
 reg_hyperparameter_stats = HyperparameterRegularizationCrossValidation.log_regressor_hyperparam_reg_cross_validate(reg_hyperparam_list=reg_hyperparameter_list, training_dataframe=pd.read_csv("data/normalized/normalized_parkinsons_training.data"))
 print("cross-validation on logistic regressor regularization hyperparameter")
-print(reg_hyperparameter_stats)"""
+print(reg_hyperparameter_stats)
 
 
-"""
+
 #finding the regularization hyperparameter that reduces mean squared and std. dev. of error using 
 #k-folds cross validation approach
 reg_hyperparameter_stats = HyperparameterRegularizationCrossValidation.lin_regressor_hyperparam_reg_cross_validate(LOGISTIC_REG_HYPERPARAMETER, reg_hyperparameter_list, training_dataframe=pd.read_csv("data/normalized/normalized_parkinsons_training.data"))
 print("cross-validation on linear regressor regularization hyperparameter")
-print(reg_hyperparameter_stats)"""
+print(reg_hyperparameter_stats)
 
 
 
