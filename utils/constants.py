23686bf69ff214fdf8865b6dc9976caba47047a1
polusy <marchisella.p@gmail.com>
2026-02-18 11:32:25 +0100

    added balanced dataset cross validation new hyperaparametr

--- utils/constants.py
+++ utils/constants.py
@@ -3,10 +3,15 @@
 NUM_INSTANCE_FEATURES = 22
 
 
-#regularization hyperparameters, found after cross-validation
+#regularization hyperparameters, found after cross-validation, for unbalanced dataset
 LOGISTIC_REG_HYPERPARAMETER = 10
 LINEAR_REG_HYPERPARAMETER = 10
 NUM_BOOSTING_ROUNDS = 2
+
+#regularization hyperparameters, found after cross-validation, for balanced dataset
+BAL_LOGISTIC_REG_HYPERPARAMETER = 10
+BAL_LINEAR_REG_HYPERPARAMETER = 10
+BAL_NUM_BOOSTING_ROUNDS = 2
 
 #learning rate hyperparameter (fixed, not found with cross-validation)
 LEARNING_RATE = 0.01
